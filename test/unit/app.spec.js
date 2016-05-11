c3d20f9a09456f6339c9b7ccce50f882d2fdb86e
Maciej Klimczyk <jackson93.07@gmail.com>
2016-05-11 14:01:01 +0200

    I made some new flawless tests

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -19,6 +19,30 @@ describe('app', function () {
                 name: 'Zack', age: 5, course: 'kids'
             });
         });
+        xit('should assign person below 12 to kids group', function() {
+            expect(app.calculateAge('15/07/2010')).toEqual({
+                name: 'Paul', age: 5, course: 'kids'
+            });
+        });
+    });
+
+    describe('calculateArea', function() {
+        it('should return {area: 1, message: "Success" if area is 1', function() {
+            expect(app.calculateArea(12, 11, 1, 'Success', 'Failure')).toEqual({area: 1, message: 'Success'});
+        });
+        it('should return {area: 5, message: "Success"} if a and b are reversed', function() {
+            expect(app.calculateArea(7, 2, 1, "Success", 'Failure')).toEqual({area: 5, message: 'Success'});
+            expect(app.calculateArea(2, 7, 1, "Success", 'Failure')).toEqual({area: 5, message: 'Success'});
+        });
+        xit("should return false when a or b or c is not numeric", function() {
+
+        });
+    });
+
+    xdescribe('calculateAge', function() {
+        it('should return 15 if a person is 15 years old', function() {
+            expect(app.calculateAge("07/10/2002")).toEqual(15);
+        });
     });
 });
 
