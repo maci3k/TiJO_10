c01d095e321b782582ac55a6a3f8de0d9dfa4978
Maciej Klimczyk <jackson93.07@gmail.com>
2016-05-12 15:35:06 +0200

    Fixed bugs

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -19,8 +19,8 @@ describe('app', function () {
                 name: 'Zack', age: 5, course: 'kids'
             });
         });
-        xit('should assign person below 12 to kids group', function() {
-            expect(app.calculateAge('15/07/2010')).toEqual({
+        it('should assign person below 12 to kids group', function() {
+            expect(app.assignToSwimmingCourse('Paul', '07/15/2010')).toEqual({
                 name: 'Paul', age: 5, course: 'kids'
             });
         });
@@ -31,17 +31,17 @@ describe('app', function () {
             expect(app.calculateArea(12, 11, 1, 'Success', 'Failure')).toEqual({area: 1, message: 'Success'});
         });
         it('should return {area: 5, message: "Success"} if a and b are reversed', function() {
-            expect(app.calculateArea(7, 2, 1, "Success", 'Failure')).toEqual({area: 5, message: 'Success'});
-            expect(app.calculateArea(2, 7, 1, "Success", 'Failure')).toEqual({area: 5, message: 'Success'});
+            expect(app.calculateArea(7, 2, 1, 'Success', 'Failure')).toEqual({area: 5, message: 'Success'});
+            expect(app.calculateArea(2, 7, 1, 'Success', 'Failure')).toEqual({area: 5, message: 'Success'});
         });
-        xit("should return false when a or b or c is not numeric", function() {
+        it('should return false when a or b or c is not numeric', function() {
 
         });
     });
 
-    xdescribe('calculateAge', function() {
+    describe('calculateAge', function() {
         it('should return 15 if a person is 15 years old', function() {
-            expect(app.calculateAge("07/10/2002")).toEqual(15);
+            expect(app.calculateAge('07/10/2002')).toEqual(13);
         });
     });
 });
